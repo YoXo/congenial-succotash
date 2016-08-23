4af2941098896a41a349262dc1ea346fad8e9186
YoXo <yaoxu12345@gmail.com>
2016-08-23 16:08:23 -0400

    Add files via upload

---
+++ messenger/src/main/java/org/RESTful/messenger/exception/DataNotFoundException.java
@@ -0,0 +1,11 @@
+package org.RESTful.messenger.exception;
+
+public class DataNotFoundException extends RuntimeException
+{
+
+	public DataNotFoundException(String message)
+	{
+		super(message);
+	}
+
+}
---
+++ messenger/src/main/java/org/RESTful/messenger/exception/GenericExceptionMapper.java
@@ -0,0 +1,25 @@
+package org.RESTful.messenger.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.RESTful.messenger.model.ErrorMessage;
+
+// This class intentionally doesn't have the @Provider annotation.
+// It has been disabled in order to try out other ways of throwing exceptions in JAX-RS
+
+// @Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable>
+{
+
+	@Override
+	public Response toResponse(Throwable ex)
+	{
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 500,
+				"https://docs.oracle.com/javase/7/docs/api/java/lang/Exception.html");
+		return Response.status(Status.INTERNAL_SERVER_ERROR)
+				.entity(errorMessage).build();
+	}
+
+}
